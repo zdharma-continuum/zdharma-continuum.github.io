11e9eff0c1c24d387c6a99f1a7408f71fb2921f3
Sebastian Gniazdowski <sgniazdowski@gmail.com>
2017-03-12 10:31:07 +0100

    build-dev: Replace domain with localhost:4000

--- build-dev.sh
+++ build-dev.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-sh ./generate-static.sh &&
+sh ./generate-static.sh
+( cd public/assets/js; perl -i -p -e 's/zdharma.org/localhost:4000/g;' bundle.js )
 bundle exec jekyll serve --config "_config.yml,_config_dev.yml" --incremental
